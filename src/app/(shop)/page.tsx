7d94a4e710fb27fa9ced0c58b804038483a5baba
“tyraiel” <tyraiel@gmail.com>
2024-05-10 14:33:58 -0300

    componente title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,13 @@
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hola Mundo</h1>
-      <h1 className={ `${titleFont.className} font-bold` }>Hola Mundo</h1>
-      <h1 className={ `${titleFont.className}` }>Hola Mundo</h1>
-    </main>
+    <>
+    <Title 
+    title="Tienda"
+    subtitle="Todos los productos"
+    className="mb-2"
+    />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@
 
 
 export * from './ui/top-menu/TopMenu';
-export * from './ui/not-found/PageNotFound'
+export * from './ui/not-found/PageNotFound';
+export * from './ui/title/Title';
