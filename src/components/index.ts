2e9cbe01191f6e22a7967382c9c7fe21f360a60c
“tyraiel” <tyraiel@gmail.com>
2024-05-12 20:19:28 -0300

    slideshow de imagenes

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export * from './products/product-grid/ProductGridItem';
 
 export * from './product/size-selector/SizeSelector';
 export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideshow';
 
 
 export * from './ui/sidebar/Sidebar';
