8b8245f5eab5d1f80b0631ca15e26d3039336a37
“tyraiel” <tyraiel@gmail.com>
2024-05-12 21:22:48 -0300

    slidesshow mobile

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -24,9 +24,17 @@ export default function({ params }: Props) {
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       
       <div className="col-span-1 md:col-span-2">
-        <ProductSlideshow 
+
+        <ProductMobileSlideshow 
         title={ product.title }
         images={ product.images }
+        className="block md:hidden"
+        />
+
+       <ProductSlideshow 
+        title={ product.title }
+        images={ product.images }
+        className="hidden md:block"
         />
       </div>
 
