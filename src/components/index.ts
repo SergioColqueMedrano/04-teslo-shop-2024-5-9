47e01e57ed4abf5a2870b3118df998dcf263dbf3
“tyraiel” <tyraiel@gmail.com>
2024-05-13 21:27:41 -0300

    Footer - Pie de página

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
+export * from './ui/footer/Footer';
 
 export * from './product/size-selector/SizeSelector';
 export * from './product/quantity-selector/QuantitySelector';
