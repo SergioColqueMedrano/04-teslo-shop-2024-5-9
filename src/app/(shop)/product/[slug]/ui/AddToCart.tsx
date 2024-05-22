6e07ef0c385990c0edbb32a34fcd6623a5c863ce
“tyraiel” <tyraiel@gmail.com>
2024-05-22 15:33:50 -0300

    Mensaje de error si no hay talla seleccionada

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -12,10 +12,32 @@ interface Props {
 
 export const AddToCart = ({ product }: Props) => {
 
-    const [size, setSize] = useState<Size | undefined>()
+    const [size, setSize] = useState<Size | undefined>();
+    const [quantity, setQuantity] = useState<number>(1);
+    
+    const [posted, setPosted] = useState(false);
+
+    const addToCart = () => {
+        setPosted(true);
+
+        if (!size) return;
+
+        console.log({size, quantity});
+    }
+
 
   return (
     <>
+        {
+            posted && !size && (
+                <span className="mt-2 text-red-500 fade-in">
+                    Debe de seleccionar una talla*
+                </span>
+            )
+        }
+        
+
+
     <SizeSelector 
           selectedSize={ size }
           availableSizes={ product.sizes }
@@ -23,11 +45,14 @@ export const AddToCart = ({ product }: Props) => {
           />
 
           <QuantitySelector
-            quantity={ 2 }
+            quantity={ quantity }
+            onQuantityChanged={setQuantity}
           />
 
 
-          <button className="btn-primary my-5">
+          <button 
+          onClick={ addToCart }
+          className="btn-primary my-5">
             Agregar al carrito
           </button>
     </>
