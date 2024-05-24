9ca7bd086f0ed5244c345093bceb5ed5527dfe27
“tyraiel” <tyraiel@gmail.com>
2024-05-24 10:14:20 -0300

    Mostrar errores en pantalla

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import clsx from "clsx";
 import Link from "next/link"
 import { SubmitHandler, useForm } from "react-hook-form";
 
@@ -21,16 +22,23 @@ export const RegisterForm = () => {
 
     return (
     <form onSubmit={ handleSubmit(onSubmit) } className="flex flex-col">
-        {
+        {/*
             errors.name?.type === 'required' && (
                 <span className="text-red-500">* El nombre es obligatorio</span>
             )
-        }
+        */}
 
 
       <label htmlFor="email">Nombre completo</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          className={
+            clsx(
+                "px-5 py-2 border bg-gray-200 rounded mb-5",
+                {
+                    'border-red-500': errors.name
+                }
+            )
+          }
           type="text"
           autoFocus 
           {...register('name', {required: true})}
@@ -39,7 +47,14 @@ export const RegisterForm = () => {
 
         <label htmlFor="email">Correo electrónico</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          className={
+            clsx(
+                "px-5 py-2 border bg-gray-200 rounded mb-5",
+                {
+                    'border-red-500': errors.email
+                }
+            )
+          }
           type="email" 
           {...register('email', {required: true, pattern: /^\S+@\S+$/i })}
           />
@@ -47,9 +62,16 @@ export const RegisterForm = () => {
 
         <label htmlFor="email">Contraseña</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          className={
+            clsx(
+                "px-5 py-2 border bg-gray-200 rounded mb-5",
+                {
+                    'border-red-500': errors.password
+                }
+            )
+          }
           type="password" 
-          {...register('password', {required: true})}
+          {...register('password', {required: true, minLength: 6})}
           />
 
         <button
