7c11bf6d18f74187dcdaf04676b616097ef3b6bf
“tyraiel” <tyraiel@gmail.com>
2024-05-24 10:39:00 -0300

    Server Action - Crear usuario

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { registerUser } from "@/actions";
 import clsx from "clsx";
 import Link from "next/link"
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 type FormInputs = {
@@ -13,20 +15,25 @@ type FormInputs = {
 
 export const RegisterForm = () => {
 
+    const [errorMessage, setErrorMessage] = useState('')
     const {register, handleSubmit, formState: {errors} } = useForm<FormInputs>();
 
     const onSubmit: SubmitHandler<FormInputs> = async(data) => {
+        setErrorMessage('');
         const {name, email, password} = data;
-        console.log({name,email,password});
+        const resp = await registerUser(name,email,password);
+
+        if ( !resp.ok) {
+            setErrorMessage( resp.message );
+            return;
+        }
+
+        console.log({resp});
     }
 
     return (
     <form onSubmit={ handleSubmit(onSubmit) } className="flex flex-col">
-        {/*
-            errors.name?.type === 'required' && (
-                <span className="text-red-500">* El nombre es obligatorio</span>
-            )
-        */}
+        
 
 
       <label htmlFor="email">Nombre completo</label>
@@ -73,6 +80,10 @@ export const RegisterForm = () => {
           type="password" 
           {...register('password', {required: true, minLength: 6})}
           />
+
+           
+                <span className="text-red-500">{ errorMessage } </span>
+            
 
         <button
           
