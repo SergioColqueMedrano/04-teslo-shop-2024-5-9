6d7925e3b3827c09e4a2df48c014624e857253b7
“tyraiel” <tyraiel@gmail.com>
2024-05-26 18:26:32 -0300

    Server Action - Guardar y actualizar dirección

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,7 @@ export * from './product/get-product-by-slug'
 export * from './product/get-stock-by-slug'
 
 export * from './country/get-countries'
+export * from './address/set-user-address'
 
 export * from './auth/login'
 export * from './auth/logout'
