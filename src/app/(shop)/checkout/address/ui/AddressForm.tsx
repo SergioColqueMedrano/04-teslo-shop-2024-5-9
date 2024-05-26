4cf1e98529b2d4f70ec586aa6157928c8a4b3acf
“tyraiel” <tyraiel@gmail.com>
2024-05-26 19:15:04 -0300

    Guardar y eliminar la dirección

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form"
 import type { Country } from "@/interfaces";
 import { useAddressStore } from "@/store";
 import { useEffect } from "react";
+import { setUserAddress } from "@/actions";
+import { useSession } from "next-auth/react";
 
 type FormInputs = {
     firstName: string;
@@ -30,6 +32,10 @@ export const AddressForm = ({countries}: Props) => {
     }
   });
 
+  const {data: session} = useSession({
+    required: true,
+  })
+
   const setAddress = useAddressStore( state => state.setAddress )
   const address = useAddressStore( state => state.address )
 
@@ -41,9 +47,19 @@ export const AddressForm = ({countries}: Props) => {
   
 
   const onSubmit = (data: FormInputs) => {
-    console.log(data);
+
 
     setAddress(data);
+    const { rememberAddress, ...restAddress} = data;
+
+    if (rememberAddress){
+      setUserAddress(restAddress, session!.user.id)
+
+
+    } else {
+      
+    }
+
   }
 
 
