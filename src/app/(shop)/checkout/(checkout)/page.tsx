db685fd9603c904326d64838266f475ddfe0e280
“tyraiel” <tyraiel@gmail.com>
2024-05-27 11:04:12 -0300

    Pantalla de confirmación - Parte 1

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,13 +1,9 @@
 import { Title } from "@/components";
-import { initialData } from "@/seed/seed";
 import Link from "next/link";
 import Image from "next/image";
+import { ProductsInCart } from "./ui/ProductsInCart";
 
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]
+
 
 export default function CheckoutPage() {
   return (
@@ -22,33 +18,8 @@ export default function CheckoutPage() {
               Editar carrito
             </Link>
           
-
-          {
-            productsInCart.map((product) =>(
-              <div key={ product.slug } className="flex mb-5">
-                <Image
-                src={ `/products/${ product.images[0] }` }
-                width={ 100 }
-                height={ 100 }
-                style={{
-                  width: '100px',
-                  height: '100px'
-                }}
-                alt={ product.title }
-                className="mr-5 rounded"
-                />
-
-                <div>
-                  <p>{ product.title }</p>
-                  <p>{ product.price }  x 3</p>
-                  <p className="font-bold">Subtotal: ${ product.price * 3}</p>
-                  <button className="underline mt-3">
-                    Remover
-                  </button>
-                </div>
-              </div>
-            ))
-          }
+            <ProductsInCart/>
+          
           </div>
 
           <div className="bg-white rounded-xl shadow-xl p-7">
