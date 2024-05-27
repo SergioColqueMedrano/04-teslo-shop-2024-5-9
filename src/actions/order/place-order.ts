29200af1c0a1415ffbed81300961fdc1559983c1
“tyraiel” <tyraiel@gmail.com>
2024-05-27 18:39:56 -0300

    Colocar orden - Productos y cantidades

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -27,16 +27,16 @@ export const placeOrder = async (
 
   // Obtener la información de los productos
   // Nota: recuerden que podemos llevar 2+ productos con el mismo ID
-  //const products = await prisma.product.findMany({
-    //where: {
-//id: {
-     //   in: productIds.map((p) => p.productId),
-     // },
-    //},
- // });
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((p) => p.productId),
+      },
+    },
+  });
 
   // Calcular los montos // Encabezado
-  //const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
 
   // Los totales de tax, subtotal, y total
   //const { subTotal, tax, total } = productIds.reduce(
