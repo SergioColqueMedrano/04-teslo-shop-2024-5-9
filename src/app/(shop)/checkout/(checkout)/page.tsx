e75bf4eca0ab89d62c775cc5ed82c8df7d64820c
“tyraiel” <tyraiel@gmail.com>
2024-05-27 11:35:20 -0300

    Pantalla de confirmación - Parte 2

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -2,6 +2,7 @@ import { Title } from "@/components";
 import Link from "next/link";
 import Image from "next/image";
 import { ProductsInCart } from "./ui/ProductsInCart";
+import { PlaceOrder } from "./ui/PlaceOrder";
 
 
 
@@ -14,7 +15,7 @@ export default function CheckoutPage() {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           <div className="flex flex-col mt-5">
             <span className="text-xl">Ajustar elementos</span>
-            <Link href="/" className="underline mb-5">
+            <Link href="/cart" className="underline mb-5">
               Editar carrito
             </Link>
           
@@ -22,51 +23,7 @@ export default function CheckoutPage() {
           
           </div>
 
-          <div className="bg-white rounded-xl shadow-xl p-7">
-              <h2 className="text-2xl font-bold mb-2">Dirección de entrega</h2>
-              <div className="mb-10">
-                <p className="text-xl">Fernando Herrera</p>
-                <p>Av. Siempre viva 123</p>
-                <p>Col. Centro</p>
-                <p>Alcadía Cuauhtémoc</p>
-                <p>Ciudad de México</p>
-                <p>CP 123123</p>
-                <p>123.123.123</p>
-
-              </div>
-
-              <div className="w-full h-0.5 rounded bg-gray-200 mb-10"/>
-
-              <h2 className="text-2xl mb-2">Resumen de orden</h2>
-              <div className="grid grid-cols-2">
-                  <span>No. Productos</span>
-                  <span className="text-right">3 articulos</span>
-
-                  <span>Subtotal</span>
-                  <span className="text-right">$ 100</span>
-
-                  <span>Impuestos (15%)</span>
-                  <span className="text-right">$ 100</span>
-
-                  <span className="mt-5 text-2xl">Total</span>
-                  <span className="mt-5 text-2xl text-right">$ 100</span>
-              </div>
-                  
-              <div className="mt-5 mb-1 w-full">
-                  <p className="mb-5">
-                    <span className="text-xs">
-                      Al hacer clic en `&quot1;`Colocar orden`&quot1;`, aceptas nuestros <a href="#" className="underline">términos</a>
-                    </span>
-
-                  </p>
-
-                  <Link 
-                  className="flex btn-primary justify-center"
-                  href="/orders/123">
-                    Colocar orden
-                  </Link>
-              </div>
-          </div>
+          <PlaceOrder/>
         </div>
 
       </div>
