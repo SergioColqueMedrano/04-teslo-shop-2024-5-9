a9b42a7455bdfb982283739968c83d7961a1dcd5
“tyraiel” <tyraiel@gmail.com>
2024-05-29 09:25:35 -0300

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+  };
+  
+  module.exports = nextConfig;
